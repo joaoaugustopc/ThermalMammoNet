011b520fc504e4cc256ae20bca03875de494f94b
joaoaugustopc <joaoaugusto.jf@gmail.com>
2026-01-03 16:31:33 +0000

    modelos DatasetSegFixedTagTemp

--- run_models.sh
+++ run_models.sh
@@ -6,43 +6,43 @@ set -e  # se der erro em algum experimento, para tudo
 
 echo ">>> Rodando modelo $i do dataset original..."
 
-docker run --rm \
-  --gpus all \
-  --name thermal-img-experiment \
-  -v "$(pwd):/experiment" \
-  fix_ufpe_images \
-  python -m main \
-    --raw_root "filtered_raw_dataset" \
-    --angle "Frontal" \
-    --k 5 \
-    --resize_to 224 \
-    --n_aug 2 \
-    --batch 8 \
-    --message "unet_marcador" \
-    --resize_method "BlackPadding" \
-    --segment "unet"
+# docker run --rm \
+#   --gpus all \
+#   --name thermal-img-experiment \
+#   -v "$(pwd):/experiment" \
+#   fix_ufpe_images \
+#   python -m main \
+#     --raw_root "filtered_raw_dataset" \
+#     --angle "Frontal" \
+#     --k 5 \
+#     --resize_to 224 \
+#     --n_aug 2 \
+#     --batch 8 \
+#     --message "unet_marcador" \
+#     --resize_method "BlackPadding" \
+#     --segment "unet"
 
 
-# for i in {0..5}; do
-#   echo ">>> Rodando modelo $i do dataset original..."
+for i in {0..5}; do
+  echo ">>> Rodando modelo $i do dataset original..."
 
-#   docker run --rm \
-#     --gpus all \
-#     --name thermal-img-experiment \
-#     -v "$(pwd):/experiment" \
-#     fix_ufpe_images \
-#     python -m main \
-#       --raw_root "filtered_raw_dataset" \
-#       --angle "Frontal" \
-#       --k 5 \
-#       --resize_to 224 \
-#       --n_aug 2 \
-#       --batch 8 \
-#       --message "Vgg_AUG_CV_DatasetSegYolo2classes_t${i}" \
-#       --resize_method "BlackPadding" \
-#       --segmenter_model "yolo" \
-#       --seg_model_path "runs/segment/train40/weights/best.pt"
-# done
+  docker run --rm \
+    --gpus all \
+    --name thermal-img-experiment \
+    -v "$(pwd):/experiment" \
+    fix_ufpe_images \
+    python -m main \
+      --raw_root "filtered_raw_dataset" \
+      --angle "Frontal" \
+      --k 5 \
+      --resize_to 224 \
+      --n_aug 2 \
+      --batch 8 \
+      --message "Vgg_AUG_CV_DatasetSegFixedTagTemp_t${i}" \
+      --resize_method "BlackPadding" \
+      --segmenter_model "yolo" \
+      --seg_model_path "train2/weights/best.pt"
+done
 
 
 
