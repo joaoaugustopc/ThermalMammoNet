b19a2708079748990a41db17548e7f4d2f9d9876
joaoaugustopc <joaoaugusto.jf@gmail.com>
2026-01-10 15:44:17 +0000

    testando run_models

--- run_models.sh
+++ run_models.sh
@@ -4,27 +4,43 @@ set -e  # se der erro em algum experimento, para tudo
 
 # 1) Primeira leva de 6 modelos (dataset original)
 
-echo ">>> Rodando modelo $i do dataset original..."
 
-# docker run --rm \
-#   --gpus all \
-#   --name thermal-img-experiment \
-#   -v "$(pwd):/experiment" \
-#   fix_ufpe_images \
-#   python -m main \
-#     --raw_root "filtered_raw_dataset" \
-#     --angle "Frontal" \
-#     --k 5 \
-#     --resize_to 224 \
-#     --n_aug 2 \
-#     --batch 8 \
-#     --message "unet_marcador" \
-#     --resize_method "BlackPadding" \
-#     --segment "unet"
+# for i in {0..2}; do
+#   docker run --rm \
+#     --gpus all \
+#     --name thermal-img-experiment \
+#     -v "$(pwd):/experiment" \
+#     fix_ufpe_images \
+#     python -m main \
+#       --raw_root "Frontal_temp_fixa_txt_rounded" \
+#       --angle "Frontal" \
+#       --k 5 \
+#       --resize_to 224 \
+#       --n_aug 2 \
+#       --batch 8 \
+#       --message "Vgg_AUG_CV_DatasetFixedTagTemp_09_01t${i}_teste" \
+#       --resize_method "BlackPadding"
+# done
+
+# for i in {0..2}; do
+#   docker run --rm \
+#     --gpus all \
+#     --name thermal-img-experiment \
+#     -v "$(pwd):/experiment" \
+#     fix_ufpe_images \
+#     python -m main \
+#       --raw_root "processed_images_padmovido_txt_rounded" \
+#       --angle "Frontal" \
+#       --k 5 \
+#       --resize_to 224 \
+#       --n_aug 2 \
+#       --batch 8 \
+#       --message "Vgg_AUG_CV_DatasetMarcadorMovidoFixo_09_01_t${i}_teste" \
+#       --resize_method "BlackPadding"
+# done
 
 
-for i in {0..5}; do
-  echo ">>> Rodando modelo $i do dataset original..."
+for i in {0..2}; do
 
   docker run --rm \
     --gpus all \
@@ -38,46 +54,26 @@ for i in {0..5}; do
       --resize_to 224 \
       --n_aug 2 \
       --batch 8 \
-      --message "Vgg_AUG_CV_DatasetSegFixedTagTemp_t${i}" \
+      --message "Vgg_AUG_CV_DatasetSegYolo_09_01_t${i}_teste" \
       --resize_method "BlackPadding" \
       --segmenter_model "yolo" \
-      --seg_model_path "train2/weights/best.pt"
+      --seg_model_path "runs/segment/train39/weights/best.pt"
 done
 
 
-
-
-# #!/bin/bash
-# set -e  # se der erro em algum experimento, para tudo
-
-# # 1) Primeira leva de 6 modelos (dataset original)
-# for i in {2..5}; do
-#   echo ">>> Rodando modelo $i do dataset original..."
-
-#   docker compose run --rm thermal-img-experiment \
+# for i in {0..2}; do
+#   docker run --rm \
+#     --gpus all \
+#     --name thermal-img-experiment \
+#     -v "$(pwd):/experiment" \
+#     fix_ufpe_images \
 #     python -m main \
-#       --raw_root "filtered_raw_dataset" \
+#       --raw_root "processed_images(pad 28x28px)_teste_txt_rounded" \
 #       --angle "Frontal" \
 #       --k 5 \
 #       --resize_to 224 \
 #       --n_aug 2 \
 #       --batch 8 \
-#       --message "Vgg_AUG_CV_DatasetOriginal_t${i}" \
-#       --resize_method "BlackPadding"
-# done
-
-# # 2) Segunda leva de 6 modelos (dataset processado)
-# for i in {0..5}; do
-#   echo ">>> Rodando modelo $i do dataset com tags..."
-
-#   docker compose run --rm thermal-img-experiment \
-#     python -m main \
-#       --raw_root "processed_images(pad 28x28px)_teste_txt" \
-#       --angle "Frontal" \
-#       --k 5 \
-#       --resize_to 224 \
-#       --n_aug 2 \
-#       --batch 8 \
-#       --message "Vgg_AUG_CV_DatasetTagFixedTam_t${i}" \
+#       --message "Vgg_AUG_CV_DatasetTagFixedTam_09_01_t${i}_teste" \
 #       --resize_method "BlackPadding"
 # done
